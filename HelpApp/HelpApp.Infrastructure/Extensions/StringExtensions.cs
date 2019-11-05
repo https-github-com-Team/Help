c22312032071b26cb4a4e626a7dada0b3addf458
elshadzr@code.edu.az <elshadzr@code.edu.az>
2019-11-05 16:01:05 +0400

    Test

--- HelpApp/HelpApp.Infrastructure/Extensions/StringExtensions.cs
+++ HelpApp/HelpApp.Infrastructure/Extensions/StringExtensions.cs
@@ -6,9 +6,6 @@ namespace HelpApp.Infrastructure.Extensions
 {
     public static class StringExtensions
     {
-        public static string Test(this string str,string val)
-        {
-            return str + val;
-        }
+       
     }
 }
