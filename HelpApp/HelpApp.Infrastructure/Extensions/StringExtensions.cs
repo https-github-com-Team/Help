a31fffce30cc3461fb311888c751cdbc9a60d5a9
elshadzr@code.edu.az <elshadzr@code.edu.az>
2019-11-05 15:57:48 +0400

    Test

---
+++ HelpApp/HelpApp.Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpApp.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Test(this string str,string val)
+        {
+            return str + val;
+        }
+    }
+}
